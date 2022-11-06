9498767d19669c0ccfb75f1c3407216867ab7a48
Alok pal <98938279+alokpall@users.noreply.github.com>
2022-11-06 18:03:22 +0700

    Add files via upload

---
+++ movies/src/App.js
@@ -0,0 +1,29 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Banner from './Components/Banner';
+import List from './Components/List';
+import Favourites from './Components/Favourites';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+function App() {
+  return (
+
+  // without router using we render in this type
+            // // <>
+            // <Navbar/>
+            // <Banner/>
+            // </List/>
+            // <Favourites/>
+            // // </>
+    <BrowserRouter>
+    <Navbar/>
+    {/* <Banner/> */}
+    <Routes>
+      <Route path="/" element={<><Banner/><List/></>}/>
+      <Route path="/fav" element={<Favourites />} />
+    </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
